7f2cc27f3dda94ec9aebed4e281326acee907e42
mengguiyouziyi <775618369@qq.com>
2017-10-13 18:14:35 +0800

    dim_logo_web

--- dim/other/id_in_hash.py
+++ dim/other/id_in_hash.py
@@ -94,40 +94,38 @@ def insert_redis():
 		mysql2.close()
 
 
-# def in_redis_id():
-# 	"""
-# 	只入id
-# 	:return:
-# 	"""
-# 	sql_config = {'host': '47.95.31.183',
-# 	             'port': 3306,
-# 	             'user': 'test',
-# 	             'password': '123456',
-# 	             # 'db': 'innotree_data_online',
-# 	             'charset': 'utf8',
-# 	             'cursorclass': pymysql.cursors.DictCursor}
-# 	mysql1 = get_mysql_con(config=sql_config)
-# 	mysql1.select_db(db='innotree_data_online')
-# 	cur1 = mysql1.cursor()
-#
-# 	try:
-# 		sql1 = """select c_name from linshi_com"""
-# 		cur1.execute(sql1)
-# 		results1 = cur1.fetchall()
-#
-# 		redis_db = get_redis_db(host='a027.hb2.innotree.org')
-# 		for result in results2:
-# 			start += 1
-# 			print(start)
-# 			if isRegister(result['only_id']):
-# 				in_redis_hash(redis_db, '2622_only_id', result['only_id'], result['be_company_name'])
-#
-# 	except:
-# 		traceback.print_exc()
-# 	finally:
-# 		mysql1.close()
+def in_redis_id():
+	"""
+	只入id
+	:return:
+	"""
+	sql_config = {'host': '47.95.31.183',
+	             'port': 3306,
+	             'user': 'test',
+	             'password': '123456',
+	             # 'db': 'innotree_data_online',
+	             'charset': 'utf8',
+	             'cursorclass': pymysql.cursors.DictCursor}
+	mysql1 = get_mysql_con(config=sql_config)
+	mysql1.select_db(db='innotree_data_online')
+	cur1 = mysql1.cursor()
+
+	try:
+		sql1 = """select c_name from linshi_com"""
+		cur1.execute(sql1)
+		results1 = cur1.fetchall()
+
+		redis_db = get_redis_db(host='a027.hb2.innotree.org')
+		for i, result in enumerate(results1):
+			print(i)
+			in_redis_hash(redis_db, '2622_only_id_all', result['c_name'], '1')
+
+	except:
+		traceback.print_exc()
+	finally:
+		mysql1.close()
 
 
 
 if __name__ == '__main__':
-	insert_redis()
+	in_redis_id()
