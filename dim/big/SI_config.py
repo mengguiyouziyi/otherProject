c8399df7ba88b724edb9d35bf609746ca22909f6
mengguiyouziyi <775618369@qq.com>
2017-10-14 10:15:59 +0800

    dim_logo_web

--- dim/big/SI_config.py
+++ dim/big/SI_config.py
@@ -48,7 +48,7 @@ sel_inser_list = [
 	# {'sel_columns': 'quan_cheng, zhuce_dizhi', 'sel_table': 'tyc_jichu_quan', 'db': 'tianyancha',
 	#  'inser_table': 'comp_registaddr_tyc', 'inser_columns': '(comp_full_name, regaddr)'},
 
-	# officeaddr
+	# office
 	# tianyancha.tyc_jichu_quan
 	{'sel_columns': 'quan_cheng, a_ddress', 'sel_table': 'tyc_jichu_quan', 'db': 'tianyancha',
 	 'inser_table': 'comp_officeaddr_tyc', 'inser_columns': '(comp_full_name, offaddr)'},
