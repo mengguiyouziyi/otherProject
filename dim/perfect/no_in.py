33574a54b494ca04d798913a395309675e3e8c95
mengguiyouziyi <775618369@qq.com>
2017-10-14 16:26:05 +0800

    dim_logo_web

--- dim/perfect/no_in.py
+++ dim/perfect/no_in.py
@@ -34,28 +34,39 @@ while True:
 	if not res:
 		break
 	sta += len(res)
-	print(res[0])
+	# print(res[0])
 	results.extend(res)
-print(results[0])
+# print(results[0])
 result_dict = {result['comp_id']: result['comp_full_name'] for result in results}  # 做成id：name的全集字典
 
 # 全量取出redis中的 comp_id, comp_full_name
 redis_db = get_redis_db(host='a027.hb2.innotree.org')
 id_names = get_redis_allhash(redis_db, 'intro_all_only_id')
 id_name = {k.decode('utf-8'): v.decode('utf-8') for k, v in id_names.items()}
-print(type(id_name))
+# print(type(id_name))
 
 # 取两个的差集（redis - company_base_info）
 id_cha_list = list(set(id_name.keys()) ^ set(result_dict.keys()))
-print(id_cha_list[:3])
+# print(id_cha_list[:3])
 # chas = [{'comp_id': cha, 'comp_full_name': id_name[cha]} for cha in cha_list]
 # cha_dict = {cha: id_name[cha] for cha in cha_list}
 
 # redis_dict = {'comp_id': id_name, 'comp_full_name': id_name}
 
-# 将这部分差集放入mysql
+# 将这部分差集放入 company_base_info_nohave
 in_sql = """insert into company_base_info_nohave (comp_id, comp_full_name) VALUES (%s, %s)"""
 value_list = [[id, id_name[id]] for id in id_cha_list]
-print(value_list[0])
+# print(value_list[0])
+m = []
+for i, n in enumerate(value_list):
+	m.append(n)
+	print(i)
+	if len(m) == 50000:
+		cur.executemany(in_sql, args=value_list)
+		con.commit()
+		m.clear()
+	else:
+		continue
 cur.executemany(in_sql, args=value_list)
 con.commit()
+con.close()
