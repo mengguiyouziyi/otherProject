251b5c7c0744fff5b8688a209901171199253a08
mengguiyouziyi <775618369@qq.com>
2017-10-15 17:53:47 +0800

    dim_logo_web

--- dim/other/all_in_hash.py
+++ dim/other/all_in_hash.py
@@ -10,6 +10,7 @@ fff = os.path.dirname(ff)
 sys.path.extend([f, ff, fff])
 
 import pymysql
+import traceback
 from dim.utility.tools import get_redis_db, in_redis_hash
 
 redis_db = get_redis_db(host='a027.hb2.innotree.org')
@@ -22,22 +23,34 @@ sel_config = {'host': 'etl1.innotree.org',
               'cursorclass': pymysql.cursors.DictCursor}
 sel_mysql = pymysql.connect(**sel_config)
 sel_cur = sel_mysql.cursor()
-all_ids = 0
-for n in range(10):
-	sta = 0
-	while True:
-		all_sql = """select only_id, comp_full_name from comp_base_result{num} limit {sta}, 500000""".format(num=n,
-		                                                                                                     sta=sta)
-		sel_cur.execute(all_sql)
-		results = sel_cur.fetchall()
-		if not results:
-			break
-		for result in results:
-			comp_id = result['only_id']
-			comp_full_name = result['comp_full_name']
-			in_redis_hash(redis_db, 'id_name_all', comp_id, comp_full_name)
-			in_redis_hash(redis_db, 'name_id_all', comp_full_name, comp_id)
-		sta += len(results)
-		print(sta)
-	all_ids += sta
-	print('~~~~~~~~' + str(n) + '~~~~~~~~' + str(all_ids) + '~~~~~~~~')
+
+
+def in_redis_all():
+	all_ids = 0
+	for n in range(10):
+		sta = 0
+		while True:
+			all_sql = """select only_id, comp_full_name from comp_base_result{num} limit {sta}, 500000""".format(num=n,
+			                                                                                                     sta=sta)
+			sel_cur.execute(all_sql)
+			results = sel_cur.fetchall()
+			if not results:
+				break
+			for result in results:
+				comp_id = result['only_id']
+				comp_full_name = result['comp_full_name']
+				in_redis_hash(redis_db, 'id_name_all', comp_id, comp_full_name)
+				in_redis_hash(redis_db, 'name_id_all', comp_full_name, comp_id)
+			sta += len(results)
+			print(sta)
+		all_ids += sta
+		print('~~~~~~~~' + str(n) + '~~~~~~~~' + str(all_ids) + '~~~~~~~~')
+
+
+if __name__ == '__main__':
+	try:
+		in_redis_all()
+	except:
+		traceback.print_exc()
+	finally:
+		sel_mysql.close()
