06727e4fa20e88caa9fcb8fa1b3dbc91070cd81b
mengguiyouziyi <775618369@qq.com>
2017-10-16 13:08:22 +0800

    dim_logo_web

--- dim/perfect/huizong.py
+++ dim/perfect/huizong.py
@@ -114,7 +114,7 @@ def a(olss, i):
 if __name__ == '__main__':
 	olss = get_ids("comp_gaoxin_only_id")
 	try:
-		for i in range(5):
+		for i in range(10):
 			a(olss, i)
 	finally:
 		sel_con.close()
