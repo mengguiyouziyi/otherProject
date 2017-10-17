b59ea2c70eec2587b67bb1a4e01d431028f814b5
mengguiyouziyi <775618369@qq.com>
2017-10-17 14:24:21 +0800

    dim_logo_web

--- dim/utility/tools.py
+++ dim/utility/tools.py
@@ -306,6 +306,7 @@ if __name__ == '__main__':
 	# has = hexists(a027_db, 'id_name_all', '15745982479273366790')
 	# print(has)
 	soo = a027_db.hscan('bugx_id_only_id', 5)
+	print(soo)
 	while soo[0]:
 		print(soo[0])
 		soo = a027_db.hscan('bugx_id_only_id', soo[0], 5)
