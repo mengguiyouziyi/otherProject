cebfb8238af1e57540ba52a3b07a8bc55ef8ddbd
mengguiyouziyi <775618369@qq.com>
2017-11-08 13:10:48 +0800

    dim_logo_web

--- dim/handle_redis/all_in_hash.py
+++ dim/handle_redis/all_in_hash.py
@@ -11,11 +11,27 @@ sys.path.extend([f, ff, fff])
 
 import pymysql
 import traceback
-from dim.utility.tools import get_redis_db, in_redis_hash, in_redis_string
-from dim.utility.info import a024, a027, etl_config, xin_config, online_config
+from rediscluster import StrictRedisCluster
+# from dim.utility.tools import in_redis_hash
+# from dim.utility.info import a024, a027, etl_config, xin_config, online_config
 
-a027_db = get_redis_db(a027)
-
+startup_nodes = [{"host": "172.29.237.209", "port": "7000"},
+                 {"host": "172.29.237.209", "port": "7001"},
+                 {"host": "172.29.237.209", "port": "7002"},
+                 {"host": "172.29.237.214", "port": "7003"},
+                 {"host": "172.29.237.214", "port": "7004"},
+                 {"host": "172.29.237.214", "port": "7005"},
+                 {"host": "172.29.237.215", "port": "7006"},
+                 {"host": "172.29.237.215", "port": "7007"},
+                 {"host": "172.29.237.215", "port": "7008"}]
+rc = StrictRedisCluster(startup_nodes=startup_nodes, decode_responses=True)
+etl_config = {'host': '172.31.215.38',
+              'port': 3306,
+              'user': 'spider',
+              'password': 'spider',
+              # 'db': 'dimension_result',
+              'charset': 'utf8',
+              'cursorclass': pymysql.cursors.DictCursor}
 etl = pymysql.connect(**etl_config)
 etl.select_db('dimension_result')
 etl_cur = etl.cursor()
@@ -37,10 +53,9 @@ def in_redis_all():
 			if not results:
 				break
 			for result in results:
-				comp_id = result['only_id']
+				# comp_id = result['only_id']
 				comp_full_name = result['comp_full_name']
-				in_redis_hash(a027_db, 'id_name_all', comp_id, comp_full_name)
-				in_redis_hash(a027_db, 'name_id_all', comp_full_name, comp_id)
+				rc.sadd('zhuce_names', comp_full_name)
 			sta += len(results)
 			print(sta)
 		all_ids += sta
