ca6f3c8b349821cf05927c379d422219c4dacdfc
mengguiyouziyi <775618369@qq.com>
2017-12-07 18:32:54 +0800

    dim_logo_web

--- dim/extract_last/main/extract.py
+++ dim/extract_last/main/extract.py
@@ -4,7 +4,7 @@ import os
 import sys
 import logging
 import io
-from util.info import etl
+from .. util.info import etl
 from .config import config_list
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
