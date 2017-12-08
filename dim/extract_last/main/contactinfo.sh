cfe3f4e0351259d2f9c0fbe04cc5fb6693549181
mengguiyouziyi <775618369@qq.com>
2017-12-08 14:15:55 +0800

    dim_logo_web

--- dim/extract_last/main/contactinfo.sh
+++ dim/extract_last/main/contactinfo.sh
@@ -6,6 +6,7 @@ eval "$(pyenv init -)"
 pyenv deactivate
 pyenv activate env354
 cd /data1/spider/menggui/otherProject/dim/extract_last/main/
+# 这是传给extract.py文件的三个参数，分别是 查询表、抽取类别、游标开始数字
 tab_out="tyc_jichu_quan"
 in_cat="contactinfo"
 start_num=$(cat contactinfo.txt)
