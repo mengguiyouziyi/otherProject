dae28e38625df7ec840d900d1eca09868cccae05
mengguiyouziyi <775618369@qq.com>
2017-12-09 10:58:45 +0800

    dim_logo_web

--- dim/extract_last/main/duiwai.sh
+++ dim/extract_last/main/duiwai.sh
@@ -8,5 +8,5 @@ pyenv activate env354
 cd /data1/spider/menggui/otherProject/dim/extract_last/main/
 tab_out="tyc_out_investment"
 in_cat="duiwai"
-start_num=$(cat base.txt)
+start_num=$(cat duiwai.txt)
 nohup python extract_no_name.py ${tab_out} ${in_cat} ${start_num} >> ${in_cat}.out 2>&1 &
--- dim/extract_last/main/teaminfo.sh
+++ dim/extract_last/main/teaminfo.sh
@@ -8,5 +8,5 @@ pyenv activate env354
 cd /data1/spider/menggui/otherProject/dim/extract_last/main/
 tab_out="tyc_core_team"
 in_cat="teaminfo"
-start_num=$(cat base.txt)
+start_num=$(cat teaminfo.txt)
 nohup python extract_no_name.py ${tab_out} ${in_cat} ${start_num} >> ${in_cat}.out 2>&1 &
